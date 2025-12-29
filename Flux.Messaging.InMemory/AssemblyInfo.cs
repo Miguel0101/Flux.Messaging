3752b7c2019676dc0bc17e544b1e37fabd44b0c5
Miguel0101 <miguelmagnofn@gmail.com>
2025-12-28 21:54:53 -0300

    feat(InMemory): Added InMemory implementation module

---
+++ Flux.Messaging.InMemory/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Flux.Messaging")]
+
+namespace Flux.Messaging.InMemory;
---
+++ Flux.Messaging.InMemory/InMemoryMessageBus.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Channels;
+using Flux.Messaging.Abstractions;
+using Flux.Messaging.Core;
+
+namespace Flux.Messaging.InMemory;
+
+internal sealed class InMemoryMessageBus : IMessageBus, IAsyncDisposable
+{
+    private readonly Channel<MessageEnvelope> _channel;
+    private readonly IMessageDispatcher _dispatcher;
+    private readonly Task _processingTask;
+
+    public InMemoryMessageBus(IMessageDispatcher dispatcher)
+    {
+        _dispatcher = dispatcher;
+        _channel = Channel.CreateUnbounded<MessageEnvelope>();
+        _processingTask = ProcessAsync();
+    }
+
+    public async Task PublishAsync<T>(T message, CancellationToken ct = default)
+    {
+        if (message is null)
+            throw new ArgumentNullException(nameof(message));
+
+        await _channel.Writer.WriteAsync(MessageEnvelope.Create(message), ct);
+    }
+
+    private async Task ProcessAsync()
+    {
+        await foreach (var envelope in _channel.Reader.ReadAllAsync())
+        {
+            await _dispatcher.DispatchAsync(envelope);
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        _channel.Writer.Complete();
+        await _processingTask;
+    }
+}
