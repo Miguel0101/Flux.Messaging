d3d709bb93e90c2268fb6daf1f5c4f3f09a78050
Miguel0101 <miguelmagnofn@gmail.com>
2025-12-29 21:25:43 -0300

    update(refs): Update references

--- Flux.Messaging.Core/AssemblyInfo.cs
+++ Flux.Messaging.Core/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Flux.Messaging")]
+[assembly: InternalsVisibleTo("Flux.Messaging.Extensions.DependencyInjection")]
 [assembly: InternalsVisibleTo("Flux.Messaging.InMemory")]
 
 namespace Flux.Messaging.Core;
